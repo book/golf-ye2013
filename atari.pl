57d24e58eb2e5c17dd1b6ceacee061d91ecef893
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-12 15:14:48 +0200

    remove some whitespace and comments

--- atari.pl
+++ atari.pl
@@ -1,25 +1,13 @@
 #!perl -ln
-# $b[ row ][ col ] = cell value
-$r = $b[$.] = [ '', split // ];
-
-# check left and above neighbours
-for ( 1 .. length ) {
-    if ( $b[$.][$_] eq "w" ) {    # only check white stones
-           # put in the same equivalence class if neighbour is the same colour
-        $l{"$. $_"} = $l{"@{[$.-1,$_]}"} if $b[ $. - 1 ][$_] eq $b[$.][$_];
-        $l{"$. $_"} = $l{"@{[$.,$_-1]}"} if $b[$.][ $_ - 1 ] eq $b[$.][$_];
-
-        # record liberties in the equivalence class
-        push @{ $l{"$. $_"} }, [ $_, $. - 1 ] if $b[ $. - 1 ][$_] eq " ";
-        push @{ $l{"$. $_"} }, [ $_ - 1, $. ] if $b[$.][ $_ - 1 ] eq " ";
-    }
-    elsif ( $b[$.][$_] eq " " ) {
-
-        # is this cell a liberty for a neighbouring white group?
-        push @{ $l{"@{[$.-1,$_]}"} }, [ $_, $. ] if $b[ $. - 1 ][$_] eq "w";
-        push @{ $l{"@{[$.,$_-1]}"} }, [ $_, $. ] if $b[$.][ $_ - 1 ] eq "w";
-    }
+$b[$.]=['',split//];
+for(1..length){
+if($b[$.][$_]eq"w"){
+    $l{"$. $_"} = $l{"@{[$.-1,$_]}"} if $b[ $. - 1 ][$_] eq"w";
+    $l{"$. $_"} = $l{"@{[$.,$_-1]}"} if $b[$.][ $_ - 1 ] eq"w";
+    push @{ $l{"$. $_"} }, [ $_, $. - 1 ] if $b[ $. - 1 ][$_] eq " ";
+    push @{ $l{"$. $_"} }, [ $_ - 1, $. ] if $b[$.][ $_ - 1 ] eq " ";
 }
-
-}{  # show output
-print for sort map "@{$l{$_}[0]}", grep @{ $l{$_} } == 1, keys %l;
+elsif($b[$.][$_]eq" "){
+    push @{ $l{"@{[$.-1,$_]}"} }, [ $_, $. ] if $b[ $. - 1 ][$_] eq "w";
+    push @{ $l{"@{[$.,$_-1]}"} }, [ $_, $. ] if $b[$.][ $_ - 1 ] eq "w";
+}}}{print for sort map"@{$l{$_}[0]}",grep@{$l{$_}}==1,keys%l;
