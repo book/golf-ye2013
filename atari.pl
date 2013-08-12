e5a14c612e6c2f58c1efa4a836d77a3e173872aa
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-12 23:03:30 +0200

    remove 3 characters by replacing ifs by ternaries

--- atari.pl
+++ atari.pl
@@ -1,7 +1,7 @@
 #!perl -ln
 sub
 j{push@{$l[$_[0]]},"@_[2,1]"if$_[3]eq$"?$b[$_[1].$_[2]]eq$":$b[$_[0]]eq
-w}$i=1;for(split//){$b[$..$i]=$_;$R=$.-1;$C=$i-1;{b=>sub{},w,sub{$l[$c]=$l[$R.$i]if$b[$R.$i]eq
-w;$l[$c]=$l[$..$C]if$b[$..$C]eq
-w;j$..$i,$R,$i,$";j$..$i,$.,$C,$"},$",sub{j$R.$i,$.,$i;j$..$C,$.,$i}}->{$b[$c=$..$i]||b}->();$i++}}{print
+w}$i=1;for(split//){$b[$..$i]=$_;$R=$.-1;$C=$i-1;{b=>sub{},w,sub{$l[$c]=$b[$R.$i]eq
+w?$l[$R.$i]:$b[$..$C]eq
+w?$l[$..$C]:undef;j$..$i,$R,$i,$";j$..$i,$.,$C,$"},$",sub{j$R.$i,$.,$i;j$..$C,$.,$i}}->{$b[$c=$..$i]||b}->();$i++}}{print
 for sort map$s{$_}++||@{$_}-1?():$_->[0],@l
