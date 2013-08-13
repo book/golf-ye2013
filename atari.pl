36f29e8d0af8a707215e2349371a314f46918708
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-13 09:38:09 +0200

    add new intermediate variables

--- atari.pl
+++ atari.pl
@@ -1,17 +1,15 @@
 #!perl -ln
 sub S{($m,$n)=@_;($M,$N)=sort$$m,$$n;($$m,$$n)=($M)x2;push@{$l{$M}},@{delete$l{$N}}if$N!=$M}
-$i=1;
-for(split//){
-$b[$c=$..$i]=/w/?++$e:$_;
-$R=$.-1;$C=$i-1;
-if(/w/) {
-S\$b[$c],\$b[$R.$i]if$b[$R.$i]>0;
-S\$b[$c],\$b[$..$C]if$b[$..$C]>0;#&&$b[$..$C]!=$b[$c];
-push@{$l{$b[$c]}},"$R $i"if$b[$R.$i]eq$";
-push@{$l{$b[$c]}},"$. $C"if$b[$..$C]eq$";
+$i=1;for(split//){$b[$c=$..$i]=/w/?++$e:$_;$R=$.-1;$C=$i-1;
+$x=($R=$.-1).$i;$y=$..($C=$i-1);
+if(/w/){
+S\$b[$c],\$b[$x]if$b[$x]>0;
+S\$b[$c],\$b[$y]if$b[$y]>0;#&&$b[$..$C]!=$b[$c];
+push@{$l{$b[$c]}},"$R $i"if$b[$x]eq$";
+push@{$l{$b[$c]}},"$. $C"if$b[$y]eq$";
 }elsif(/ /){
-push@{$l{$b[$R.$i]}},"$. $i"if$b[$R.$i]>0;
-push@{$l{$b[$..$C]}},"$. $i"if$b[$..$C]>0;
+push@{$l{$b[$x]}},"$. $i"if$b[$x]>0;
+push@{$l{$b[$y]}},"$. $i"if$b[$y]>0;
 }
 $i++;
 }
