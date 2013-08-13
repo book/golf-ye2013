a36bf7e9a7b14ec6d012f504fb0cdbad2a5f2861
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-13 10:19:18 +0200

    full rewrite of the script
    now the equivalence class is a number, and we merge them and their liberties on the fly

--- atari.pl
+++ atari.pl
@@ -1,6 +1,19 @@
 #!perl -ln
-sub j{push@{$l[$_[0]]},"@_[1,2]"if$_[3]eq$"?$b[$_[1].$_[2]]eq$":$b[$_[0]]eq
-w}$i=1;for(split//){$b[$..$i]=$_;$R=$.-1;$C=$i-1;{b=>sub{},w,sub{$l[$c]=$b[$R.$i]eq
-w?$l[$R.$i]:$b[$..$C]eq
-w?$l[$..$C]:$l[$c]||[];j$..$i,$R,$i,$";j$..$i,$.,$C,$"},$",sub{j$R.$i,$.,$i;j$..$C,$.,$i}}->{$b[$c=$..$i]||b}->();$i++}}{print
-for sort grep!$s{$_}++,map@$_-1?():$_->[0],@l
+sub S{($m,$n)=@_;($M,$N)=sort$$m,$$n;($$m,$$n)=($M)x2;push@{$l{$M}},@{delete$l{$N}}if$N!=$M}
+$i=1;
+for(split//){
+$b[$c=$..$i]=/w/?++$e:$_;
+$R=$.-1;$C=$i-1;
+if(/w/) {
+S\$b[$c],\$b[$R.$i]if$b[$R.$i]>0;
+S\$b[$c],\$b[$..$C]if$b[$..$C]>0;#&&$b[$..$C]!=$b[$c];
+push@{$l{$b[$c]}},"$R $i"if$b[$R.$i]eq$";
+push@{$l{$b[$c]}},"$. $C"if$b[$..$C]eq$";
+}elsif(/ /){
+push@{$l{$b[$R.$i]}},"$. $i"if$b[$R.$i]>0;
+push@{$l{$b[$..$C]}},"$. $i"if$b[$..$C]>0;
+}
+$i++;
+}
+}{
+print for sort grep!$s{$_}++,map@$_-1?():@$_,map{my%t;[grep!$t{$_}++,@$_]}values%l;
