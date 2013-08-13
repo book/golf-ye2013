791f94e6ba6cd78a37609f929d6d46f4a058228c
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-13 14:51:15 +0200

    another attempt: 425

--- atari.pl
+++ atari.pl
@@ -1,5 +1,6 @@
 #!perl -ln
-sub M{$k=pop;my%t;$l[$k]=[grep!$t{$_}++,@{$l[$k]},@_]}sub
-S{($m,$n)=@_;($M,$N)=sort$$m,$$n;M@{delete$l[$N]},$M
-if$N!=($$m=$$n=$M)}$i=1;for(split//){$b[$c=$..$i]=/w/?++$e:$_;$x=($R=$.-1).$i;$y=$..($C=$i-1);/w/&&do{$b[$_]>0&&S\$b[$c],\$b[$_]for$x,$y;M$b[$x]eq$"?"$R $i":(),$b[$y]eq$"?"$. $C":(),$b[$c]};/ /&&map{$b[$_]>0&&M"$. $i",$b[$_]}$x,$y;$i++}}{print
-for sort grep!$s{$_}++,map@$_-1?():@$_,@l
+sub L{$k=pop;my%t;$l{$b[$k]}=[grep!$t{$_}++,@{$l{$b[$k]}},@_]if
+pop()?$b[pop]eq$":$b[$k]>0}sub
+S{($m,$n)=@_;($M,$N)=sort$$m,$$n;L@{delete$l{$N}},0,$c
+if$N!=($$m=$$n=$M)}$i=1;for(split//){$b[$c=$..$i]=/w/?++$e:$_;$R=$.-1;$C=$i-1;/w/&&do{$b[$_]>0&&S\$b[$c],\$b[$_]for$R.$i,$..$C;L"$R $i",$R.$i,1,$c;L"$. $C",$..$C,1,$c};/ /&&do{L"$. $i",0,$R.$i;L"$. $i",0,$..$C};$i++}}{print
+for sort grep!$s{$_}++,map@$_-1?():@$_,values%l
