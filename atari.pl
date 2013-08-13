55171b3cf2c88c5a8c2673aeffe696b5dcf5c1fd
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-13 09:41:15 +0200

    use loops to shorten the code

--- atari.pl
+++ atari.pl
@@ -3,13 +3,11 @@ sub S{($m,$n)=@_;($M,$N)=sort$$m,$$n;($$m,$$n)=($M)x2;push@{$l{$M}},@{delete$l{$
 $i=1;for(split//){$b[$c=$..$i]=/w/?++$e:$_;$R=$.-1;$C=$i-1;
 $x=($R=$.-1).$i;$y=$..($C=$i-1);
 if(/w/){
-S\$b[$c],\$b[$x]if$b[$x]>0;
-S\$b[$c],\$b[$y]if$b[$y]>0;#&&$b[$..$C]!=$b[$c];
+$b[$_]>0&&S\$b[$c],\$b[$_]for$x,$y;
 push@{$l{$b[$c]}},"$R $i"if$b[$x]eq$";
 push@{$l{$b[$c]}},"$. $C"if$b[$y]eq$";
 }elsif(/ /){
-push@{$l{$b[$x]}},"$. $i"if$b[$x]>0;
-push@{$l{$b[$y]}},"$. $i"if$b[$y]>0;
+$b[$_]>0&&push@{$l{$b[$_]}},"$. $i"for$x,$y;
 }
 $i++;
 }
