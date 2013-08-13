98f079546eb418205a8bfc6fb5c5f04dc9c18231
Philippe Bruhat (BooK) <book@cpan.org>
2013-08-13 09:55:37 +0200

    remove 3 more characters

--- atari.pl
+++ atari.pl
@@ -1,4 +1,4 @@
 #!perl -ln
-sub S{($m,$n)=@_;($M,$N)=sort$$m,$$n;$$m=$M;$$n=$M;push@{$l{$M}},@{delete$l{$N}}if$N!=$M}
+sub S{($m,$n)=@_;($M,$N)=sort$$m,$$n;push@{$l{$M}},@{delete$l{$N}}if$N!=($$m=$$n=$M)}
 $i=1;for(split//){$b[$c=$..$i]=/w/?++$e:$_;$x=($R=$.-1).$i;$y=$..($C=$i-1);if(/w/){$b[$_]>0&&S\$b[$c],\$b[$_]for$x,$y;push@{$l{$b[$c]}},"$R $i"if$b[$x]eq$";push@{$l{$b[$c]}},"$. $C"if$b[$y]eq$"}elsif(/ /){$b[$_]>0&&push@{$l{$b[$_]}},"$. $i"for$x,$y}$i++}}{print
 for sort grep!$s{$_}++,map@$_-1?():@$_,map{my%t;[grep!$t{$_}++,@$_]}values%l
